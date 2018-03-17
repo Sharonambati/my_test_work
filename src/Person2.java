5b5662c56d63cc1cdd3e4b882c90dde9e8ed787b
SHARON AMBATI <sharoncm005@gmail.com>
2018-03-17 19:04:33 +0530

    New Git cmd files added 2.

--- src/Person2.java
+++ src/Person2.java
@@ -2,7 +2,7 @@ public class Person2 {
 
 	public static void main(String[] args) {
 		Person p = new Person();
-		p.setFname("ambati");
+		p.setFname("ambati mca");
 		p.setLname("sharon");
 		System.out.println(p);
 	}
